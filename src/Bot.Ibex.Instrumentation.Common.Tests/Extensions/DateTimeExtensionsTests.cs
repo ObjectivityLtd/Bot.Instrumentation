f5c2853369073e7614a1dec0b5332f7d14e39bb1
piotrzajac <piotr.zajac@live.com>
2019-06-06 01:01:15 +0200

    Move DateTimeExtensionsTests to common test project

--- src/Bot.Ibex.Instrumentation.Common.Tests/Extensions/DateTimeExtensionsTests.cs
+++ src/Bot.Ibex.Instrumentation.Common.Tests/Extensions/DateTimeExtensionsTests.cs
@@ -1,4 +1,4 @@
-namespace Bot.Ibex.Instrumentation.V4.Tests.Extensions
+namespace Bot.Ibex.Instrumentation.Common.Tests.Extensions
 {
     using System;
     using AutoFixture.Xunit2;
@@ -30,6 +30,31 @@ namespace Bot.Ibex.Instrumentation.V4.Tests.Extensions
 
             // Act
             var actual = dateTimeOffset.AsIso8601();
+
+            // Assert
+            actual.Should().Be(expected);
+        }
+
+        [Theory(DisplayName = "GIVEN DateTime WHEN AsIso8601 is invoked THEN expected string value is being returned")]
+        [InlineAutoData(2018, 1, 17, 16, 0, 54, 432, "2018-01-17T16:00:54.432Z")]
+        [InlineAutoData(1979, 10, 14, 9, 15, 0, 0, "1979-10-14T09:15:00Z")]
+        [InlineAutoData(1999, 12, 31, 23, 59, 59, 999, "1999-12-31T23:59:59.999Z")]
+        [InlineAutoData(2000, 1, 1, 0, 0, 0, 0, "2000-01-01T00:00:00Z")]
+        public void GIVENDateTime_WHENAsIso8601IsInvoked_THENExpectedStringIsBeingReturned(
+            int year,
+            int month,
+            int day,
+            int hour,
+            int minute,
+            int second,
+            int millisecond,
+            string expected)
+        {
+            // Arrange
+            var dateTime = new DateTime(year, month, day, hour, minute, second, millisecond, DateTimeKind.Utc);
+
+            // Act
+            var actual = dateTime.AsIso8601();
 
             // Assert
             actual.Should().Be(expected);
